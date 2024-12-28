1decb543df6ff8608f2e3f03509c97d51f77c77e
Khayam Ijaz <ijazkhayam@gmail.com>
2024-12-28 10:18:07 +0500

    update

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -6,14 +6,14 @@ const Hero = () => {
   return (
     <>
       <div className="md:w-[90%] w-full p2 h-full mx-auto flex md:flex-row flex-col-reverse md:items-center md:justify-between justify-center">
-        <div className="left w-full md:w-1/2 mt-3 md:block flex flex-col items-center">
+        <div className="left w-[95%] md:w-1/2 mt-3 md:block flex flex-col items-center mx-auto">
           <h1 className="md:text-5xl text-2xl font-semibold">Hello I'm Khayam</h1>
-          <p className="md:text-lg mt-2 text-center">
+          <p className="md:text-lg mt-2 md:text-left text-center">
             I create designs that tell stories and leave a lasting impact. From
             logos to branding, I turn your ideas into clear, memorable visuals
             that connect with your audience.
           </p>
-          <div className="btns md:w-full w-[90%] mt-3 md:mt-0 flex md:flex-row flex-col gap-3 items-center">
+          <div className="btns md:w-full w-[90%] mt-3 flex md:flex-row flex-col gap-3 items-center">
             <NavLink className="bg-[#CFEDFF] md:w-auto w-full text-center px-5 py-3 rounded-md hover:bg-[#b0e1ff] transition-all duration-300 shadow">
               Contact Me
             </NavLink>
